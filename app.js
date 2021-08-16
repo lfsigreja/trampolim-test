0ad0a0b4aa0ce683604410080d25790ecdfee8b5
Luis Igreja <lfsigreja@gmail.com>
2021-08-16 19:21:40 -0300

    Card 10

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const metro = 5;
 const km = metro * 1000;
 
 console.log(`${metro}m é equivalente a ${km}km`);
+
+function temperature(celsius) {
+    console.log(`${celsius}°C é equivalente a ${(celsius * 9/5) + 32}°F`);
+}
