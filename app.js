0a53ce6f4ee5a26b7d5a9b7028750517a44eb35b
Luis Igreja <lfsigreja@gmail.com>
2021-08-17 02:43:06 -0300

    final adjusments

--- app.js
+++ app.js
@@ -10,17 +10,14 @@ function temperature(celsius) {
 function scaleConverterFunction(valTemp, fromTemp, toTemp) {
     let K = 273.15
     valTemp = parseFloat(valTemp)
-    console.log(valTemp)
     if (fromTemp == 'C' && toTemp == 'C') {
         resultado = valTemp
         return resultado
     } else if (fromTemp == 'C' && toTemp == 'K') {
         resultado = valTemp + K;
-        console.log(resultado)
         return resultado
     } else if (fromTemp == 'C' && toTemp == 'F') {
         resultado = (valTemp * 1.8) + 32;
-        console.log(resultado)
         return resultado
     } else if (fromTemp == 'K' && toTemp == 'C') {
         resultado = valTemp - K;
@@ -50,9 +47,8 @@ function temperatureConverter(event) {
     let valTemp = document.getElementById('temp').value;
     let toTemp = document.getElementById("toScale").value;
     let fromTemp = document.getElementById("scaleSelected").value;
-    console.log(valTemp, toTemp, fromTemp)
     resultado = scaleConverterFunction(valTemp, fromTemp, toTemp);
-    console.log(resultado);
+    console.log(`O valor da conversão é ${valTemp}${fromTemp} para ${resultado}${toTemp}`);
     showResultTemp.innerHTML = resultado.toFixed(2);
 }
 
